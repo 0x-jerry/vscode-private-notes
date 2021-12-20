35f9cecac281c7b183ed81a728b9e75b1950e73a
0x-jerry <x.jerry.wang@gmail.com>
2021-12-20 14:40:45 +0800

    feat: sync current edit actions to target workspace

--- src/EncryptFsProvider.ts
+++ src/EncryptFsProvider.ts
@@ -1,58 +1,12 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-export class File implements vscode.FileStat {
-  type: vscode.FileType;
-  ctime: number;
-  mtime: number;
-  size: number;
-
-  name: string;
-  data?: Uint8Array;
-
-  constructor(name: string) {
-    this.type = vscode.FileType.File;
-    this.ctime = Date.now();
-    this.mtime = Date.now();
-    this.size = 0;
-    this.name = name;
-  }
-}
-
-export class Directory implements vscode.FileStat {
-  type: vscode.FileType;
-  ctime: number;
-  mtime: number;
-  size: number;
-
-  name: string;
-  entries: Map<string, File | Directory>;
-
-  constructor(name: string) {
-    this.type = vscode.FileType.Directory;
-    this.ctime = Date.now();
-    this.mtime = Date.now();
-    this.size = 0;
-    this.name = name;
-    this.entries = new Map();
-  }
-}
-
-export type Entry = File | Directory;
-
 export class MemFS implements vscode.FileSystemProvider {
-  root = new Directory('');
-
   get targetWorkspace() {
     return vscode.workspace.workspaceFolders?.[0];
   }
 
-  _getTargetUrl(uri: vscode.Uri) {
+  private _getTargetUrl(uri: vscode.Uri) {
     if (!this.targetWorkspace) throw vscode.FileSystemError.FileNotADirectory(uri);
 
     const newUri = vscode.Uri.from({
@@ -88,51 +42,47 @@ export class MemFS implements vscode.FileSystemProvider {
     return vscode.workspace.fs.readFile(newUri);
   }
 
-  writeFile(
+  async writeFile(
     uri: vscode.Uri,
     content: Uint8Array,
     options: { create: boolean; overwrite: boolean },
-  ): void {
-    const basename = path.posix.basename(uri.path);
-    const parent = this._lookupParentDirectory(uri);
-    let entry = parent.entries.get(basename);
-    if (entry instanceof Directory) {
+  ): Promise<void> {
+    let entry = await this._lookup(uri, true);
+
+    if (entry?.type === vscode.FileType.Directory) {
       throw vscode.FileSystemError.FileIsADirectory(uri);
     }
+
     if (!entry && !options.create) {
       throw vscode.FileSystemError.FileNotFound(uri);
     }
+
     if (entry && options.create && !options.overwrite) {
       throw vscode.FileSystemError.FileExists(uri);
     }
-    if (!entry) {
-      entry = new File(basename);
-      parent.entries.set(basename, entry);
-      this._fireSoon({ type: vscode.FileChangeType.Created, uri });
-    }
-    entry.mtime = Date.now();
-    entry.size = content.byteLength;
-    entry.data = content;
 
-    this._fireSoon({ type: vscode.FileChangeType.Changed, uri });
+    const newUri = this._getTargetUrl(uri);
+    await vscode.workspace.fs.writeFile(newUri, content);
+
+    if (!entry) {
+      this._fireSoon({ type: vscode.FileChangeType.Created, uri });
+    } else {
+      this._fireSoon({ type: vscode.FileChangeType.Changed, uri });
+    }
   }
 
   // --- manage files/folders
 
-  rename(oldUri: vscode.Uri, newUri: vscode.Uri, options: { overwrite: boolean }): void {
-    if (!options.overwrite && this._lookup(newUri, true)) {
-      throw vscode.FileSystemError.FileExists(newUri);
-    }
-
-    const entry = this._lookup(oldUri, false);
-    const oldParent = this._lookupParentDirectory(oldUri);
-
-    const newParent = this._lookupParentDirectory(newUri);
-    const newName = path.posix.basename(newUri.path);
-
-    oldParent.entries.delete(entry.name);
-    entry.name = newName;
-    newParent.entries.set(newName, entry);
+  async rename(
+    oldUri: vscode.Uri,
+    newUri: vscode.Uri,
+    options: { overwrite: boolean },
+  ): Promise<void> {
+    await vscode.workspace.fs.rename(
+      this._getTargetUrl(oldUri),
+      this._getTargetUrl(newUri),
+      options,
+    );
 
     this._fireSoon(
       { type: vscode.FileChangeType.Deleted, uri: oldUri },
@@ -140,31 +90,22 @@ export class MemFS implements vscode.FileSystemProvider {
     );
   }
 
-  delete(uri: vscode.Uri): void {
+  async delete(uri: vscode.Uri): Promise<void> {
+    await vscode.workspace.fs.delete(this._getTargetUrl(uri));
+
     const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-    const basename = path.posix.basename(uri.path);
-    const parent = this._lookupAsDirectory(dirname, false);
-    if (!parent.entries.has(basename)) {
-      throw vscode.FileSystemError.FileNotFound(uri);
-    }
-    parent.entries.delete(basename);
-    parent.mtime = Date.now();
-    parent.size -= 1;
+
     this._fireSoon(
       { type: vscode.FileChangeType.Changed, uri: dirname },
       { uri, type: vscode.FileChangeType.Deleted },
     );
   }
 
-  createDirectory(uri: vscode.Uri): void {
-    const basename = path.posix.basename(uri.path);
-    const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-    const parent = this._lookupAsDirectory(dirname, false);
+  async createDirectory(uri: vscode.Uri): Promise<void> {
+    await vscode.workspace.fs.createDirectory(this._getTargetUrl(uri));
 
-    const entry = new Directory(basename);
-    parent.entries.set(entry.name, entry);
-    parent.mtime = Date.now();
-    parent.size += 1;
+    const dirname = uri.with({ path: path.posix.dirname(uri.path) });
+
     this._fireSoon(
       { type: vscode.FileChangeType.Changed, uri: dirname },
       { type: vscode.FileChangeType.Created, uri },
@@ -173,51 +114,18 @@ export class MemFS implements vscode.FileSystemProvider {
 
   // --- lookup
 
-  private _lookup(uri: vscode.Uri, silent: false): Entry;
-  private _lookup(uri: vscode.Uri, silent: boolean): Entry | undefined;
-  private _lookup(uri: vscode.Uri, silent: boolean): Entry | undefined {
-    const parts = uri.path.split('/');
-    let entry: Entry = this.root;
+  private async _lookup(uri: vscode.Uri, silent: boolean): Promise<vscode.FileStat | undefined> {
+    const newUri = this._getTargetUrl(uri);
 
-    for (const part of parts) {
-      if (!part) {
-        continue;
-      }
-      let child: Entry | undefined;
-      if (entry instanceof Directory) {
-        child = entry.entries.get(part);
-      }
-      if (!child) {
-        if (!silent) {
-          throw vscode.FileSystemError.FileNotFound(uri);
-        } else {
-          return undefined;
-        }
-      }
-      entry = child;
+    if (!silent) {
+      return vscode.workspace.fs.stat(newUri);
     }
-    return entry;
-  }
 
-  private _lookupAsDirectory(uri: vscode.Uri, silent: boolean): Directory {
-    const entry = this._lookup(uri, silent);
-    if (entry instanceof Directory) {
-      return entry;
+    try {
+      return vscode.workspace.fs.stat(newUri);
+    } catch (error) {
+      //
     }
-    throw vscode.FileSystemError.FileNotADirectory(uri);
-  }
-
-  private _lookupAsFile(uri: vscode.Uri, silent: boolean): File {
-    const entry = this._lookup(uri, silent);
-    if (entry instanceof File) {
-      return entry;
-    }
-    throw vscode.FileSystemError.FileIsADirectory(uri);
-  }
-
-  private _lookupParentDirectory(uri: vscode.Uri): Directory {
-    const dirname = uri.with({ path: path.posix.dirname(uri.path) });
-    return this._lookupAsDirectory(dirname, false);
   }
 
   // --- manage file events
