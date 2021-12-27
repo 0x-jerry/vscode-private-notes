f9b02bb8154e464f987746edfd79297a4e3e0935
Jerry Wang <x.jerry.wang@gmail.com>
2021-12-27 23:05:54 +0800

    fix: update git status when create new file

--- src/EncryptFileDecoration.ts
+++ src/EncryptFileDecoration.ts
@@ -105,7 +105,7 @@ export class EncryptFileDecorationProvider extends Dispose implements FileDecora
     );
 
     this.addDisposable(
-      workspaceWatcher.onDidChange(() => {
+      workspaceWatcher.onDidCreate(() => {
         this.updateGitStatus();
       }),
     );
