40fe817a57d3bfdef34a22da386d2f70cfac4ec8
Jerry Wang <x.jerry.wang@gmail.com>
2021-12-30 23:08:54 +0800

    chore: update deps

--- x.release.conf.ts
+++ x.release.conf.ts
@@ -1,13 +1,13 @@
-import { defineConfig, InternalReleaseTask } from '@0x-jerry/x-release';
+import { defineConfig } from '@0x-jerry/x-release';
 
 export default defineConfig({
   sequence: [
-    'test',
-    'changelog',
-    InternalReleaseTask.updatePkg,
-    InternalReleaseTask.commit,
-    InternalReleaseTask.tag,
-    InternalReleaseTask.push,
+    'npm:test',
+    'npm:changelog',
+    'pkg.update.version',
+    'git.commit',
+    'git.tag',
+    'git.push',
     async (ctx) => {
       ctx.run(`vsce publish --yarn`);
     },
